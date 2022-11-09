b4a66e9586b1994815b62c38546fe8c063585cb2
ut1s <110339660+ut1s@users.noreply.github.com>
2022-11-09 20:05:24 +0100

    Add infrared and update manifest

--- applications/main/infrared/scenes/infrared_scene_universal.c
+++ applications/main/infrared/scenes/infrared_scene_universal.c
@@ -2,8 +2,10 @@
 
 typedef enum {
     SubmenuIndexUniversalTV,
-    SubmenuIndexUniversalAC,
     SubmenuIndexUniversalAudio,
+    SubmenuIndexUniversalProjector,
+    SubmenuIndexUniversalFan,
+    SubmenuIndexUniversalAirConditioner,
 } SubmenuIndex;
 
 static void infrared_scene_universal_submenu_callback(void* context, uint32_t index) {
@@ -21,19 +23,35 @@ void infrared_scene_universal_on_enter(void* context) {
         SubmenuIndexUniversalTV,
         infrared_scene_universal_submenu_callback,
         context);
+    submenu_set_selected_item(submenu, 0);
+
     submenu_add_item(
         submenu,
-        "Audio Players",
+        "Audio",
         SubmenuIndexUniversalAudio,
         infrared_scene_universal_submenu_callback,
         context);
+
     submenu_add_item(
         submenu,
-        "Air Conditioners",
-        SubmenuIndexUniversalAC,
+        "Projectors",
+        SubmenuIndexUniversalProjector,
         infrared_scene_universal_submenu_callback,
         context);
-    submenu_set_selected_item(submenu, 0);
+
+    submenu_add_item(
+        submenu,
+        "Fans",
+        SubmenuIndexUniversalFan,
+        infrared_scene_universal_submenu_callback,
+        context);
+
+    submenu_add_item(
+        submenu,
+        "ACs",
+        SubmenuIndexUniversalAirConditioner,
+        infrared_scene_universal_submenu_callback,
+        context);
 
     view_dispatcher_switch_to_view(infrared->view_dispatcher, InfraredViewSubmenu);
 }
@@ -47,11 +65,17 @@ bool infrared_scene_universal_on_event(void* context, SceneManagerEvent event) {
         if(event.event == SubmenuIndexUniversalTV) {
             scene_manager_next_scene(scene_manager, InfraredSceneUniversalTV);
             consumed = true;
-        } else if(event.event == SubmenuIndexUniversalAC) {
-            scene_manager_next_scene(scene_manager, InfraredSceneUniversalAC);
-            consumed = true;
         } else if(event.event == SubmenuIndexUniversalAudio) {
             scene_manager_next_scene(scene_manager, InfraredSceneUniversalAudio);
+            consumed = true;
+        } else if(event.event == SubmenuIndexUniversalProjector) {
+            scene_manager_next_scene(scene_manager, InfraredSceneUniversalProjector);
+            consumed = true;
+        } else if(event.event == SubmenuIndexUniversalFan) {
+            scene_manager_next_scene(scene_manager, InfraredSceneUniversalFan);
+            consumed = true;
+        } else if(event.event == SubmenuIndexUniversalAirConditioner) {
+            scene_manager_next_scene(scene_manager, InfraredSceneUniversalAC);
             consumed = true;
         }
     }
--- applications/main/infrared/scenes/infrared_scene_universal_projector.c
+++ applications/main/infrared/scenes/infrared_scene_universal_projector.c
@@ -2,17 +2,16 @@
 
 #include "common/infrared_scene_universal_common.h"
 
-void infrared_scene_universal_ac_on_enter(void* context) {
+void infrared_scene_universal_projector_on_enter(void* context) {
     infrared_scene_universal_common_on_enter(context);
 
     Infrared* infrared = context;
     ButtonPanel* button_panel = infrared->button_panel;
     InfraredBruteForce* brute_force = infrared->brute_force;
 
-    infrared_brute_force_set_db_filename(brute_force, EXT_PATH("infrared/assets/ac.ir"));
-
-    //TODO Improve A/C universal remote
-    button_panel_reserve(button_panel, 2, 3);
+    infrared_brute_force_set_db_filename(brute_force, EXT_PATH("infrared/assets/projectors.ir"));
+    //TODO Improve Projectors universal remote
+    button_panel_reserve(button_panel, 2, 2);
     uint32_t i = 0;
     button_panel_add_item(
         button_panel,
@@ -20,7 +19,7 @@ void infrared_scene_universal_ac_on_enter(void* context) {
         0,
         0,
         3,
-        24,
+        19,
         &I_Power_25x27,
         &I_Power_hvr_25x27,
         infrared_scene_universal_common_item_callback,
@@ -32,65 +31,39 @@ void infrared_scene_universal_ac_on_enter(void* context) {
         1,
         0,
         36,
-        24,
-        &I_Mode_25x27,
-        &I_Mode_hvr_25x27,
+        19,
+        &I_Mute_25x27,
+        &I_Mute_hvr_25x27,
         infrared_scene_universal_common_item_callback,
         context);
-    infrared_brute_force_add_record(brute_force, i++, "MODE");
+    infrared_brute_force_add_record(brute_force, i++, "MUTE");
     button_panel_add_item(
         button_panel,
         i,
         0,
         1,
         3,
-        66,
+        64,
         &I_Vol_up_25x27,
         &I_Vol_up_hvr_25x27,
         infrared_scene_universal_common_item_callback,
         context);
-    infrared_brute_force_add_record(brute_force, i++, "TEMP+");
+    infrared_brute_force_add_record(brute_force, i++, "VOL+");
     button_panel_add_item(
         button_panel,
         i,
         1,
         1,
         36,
-        66,
+        64,
         &I_Vol_down_25x27,
         &I_Vol_down_hvr_25x27,
         infrared_scene_universal_common_item_callback,
         context);
-    infrared_brute_force_add_record(brute_force, i++, "TEMP-");
-    button_panel_add_item(
-        button_panel,
-        i,
-        0,
-        2,
-        3,
-        98,
-        &I_Swing_25x27,
-        &I_Swing_hvr_25x27,
-        infrared_scene_universal_common_item_callback,
-        context);
-    infrared_brute_force_add_record(brute_force, i++, "SWING");
-    button_panel_add_item(
-        button_panel,
-        i,
-        1,
-        2,
-        36,
-        98,
-        &I_Timer_25x27,
-        &I_Timer_hvr_25x27,
-        infrared_scene_universal_common_item_callback,
-        context);
-    infrared_brute_force_add_record(brute_force, i++, "TIMER");
+    infrared_brute_force_add_record(brute_force, i++, "VOL-");
 
-    button_panel_add_label(button_panel, 6, 11, FontPrimary, "AC remote");
-    button_panel_add_label(button_panel, 20, 63, FontSecondary, "Temp");
-    button_panel_add_label(button_panel, 8, 23, FontSecondary, "Pwr");
-    button_panel_add_label(button_panel, 40, 23, FontSecondary, "Mod");
+    button_panel_add_label(button_panel, 10, 11, FontPrimary, "Projector");
+    button_panel_add_label(button_panel, 17, 60, FontSecondary, "Volume");
 
     view_set_orientation(view_stack_get_view(infrared->view_stack), ViewOrientationVertical);
     view_dispatcher_switch_to_view(infrared->view_dispatcher, InfraredViewStack);
@@ -104,10 +77,10 @@ void infrared_scene_universal_ac_on_enter(void* context) {
     }
 }
 
-bool infrared_scene_universal_ac_on_event(void* context, SceneManagerEvent event) {
+bool infrared_scene_universal_projector_on_event(void* context, SceneManagerEvent event) {
     return infrared_scene_universal_common_on_event(context, event);
 }
 
-void infrared_scene_universal_ac_on_exit(void* context) {
+void infrared_scene_universal_projector_on_exit(void* context) {
     infrared_scene_universal_common_on_exit(context);
 }
